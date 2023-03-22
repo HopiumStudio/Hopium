38eb621913abc809d503a35e07260b668778699e
Tom Wu <HopiumStudio@Gmail.com>
2023-03-22 00:53:19 +0000

    Change Controller to movement
    Added a bit of code....

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    public float speed = 1.0f;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
